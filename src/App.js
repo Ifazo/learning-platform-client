2f3cb45b3a785ee0f9133d7d27ed86ca4acaa3c4
ifazo <ziaulkarimifaz@gmail.com>
2022-10-27 00:08:54 +0600

    google auth added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
-import Main from './outlet/Main';
 import { routes } from './routes/Routes';
 
 function App() {
+
   return (
     <div className="App">
       <RouterProvider router={routes}/>
+
+      
+
   </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,40 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { Link } from 'react-router-dom';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import app from '../firebase/firebaseConfig'
+const auth = getAuth(app);
 
 const Header = () => {
+
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user.displayName);
+      })
+      .catch(error => {
+        console.error(error);
+    })
+  }
+
     return (
         <div>
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Tutorial Wiki</Navbar.Brand>
+        <Navbar.Brand to="/">Tutorial Wiki</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/blog">Blog</Nav.Link>
+                <Link className='btn btn-primary' to="/">Home</Link>
+                <Link className='btn btn-primary' to="/blog">Blog</Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/login">Log In</NavDropdown.Item>
-              <NavDropdown.Item href="/register">Register</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">FAQ</NavDropdown.Item>
+                  <Link onClick={handleGoogleSignIn} className='btn btn-primary' to="/login">Log In</Link>
+                  <Link className='btn btn-primary' to="/register">Register</Link>
+                  <Link className='btn btn-primary' to="#action/3.3">FAQ</Link>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
