6702b1cb77ed3a362bc16e2fc89817638fc1316c
ifazo <ziaulkarimifaz@gmail.com>
2022-10-27 01:07:50 +0600

    github auth added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,26 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import app from '../firebase/firebaseConfig'
+import { useState } from 'react';
 const auth = getAuth(app);
 
 const Header = () => {
 
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({
+
+  })
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user.displayName);
       })
       .catch(error => {
@@ -23,20 +31,43 @@ const Header = () => {
     })
   }
 
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+  }
+
     return (
         <div>
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand to="/">Tutorial Wiki</Navbar.Brand>
+            <Navbar.Brand to="/">Tutorial Wiki</Navbar.Brand>
+            <div className='d-flex'>
+              <p>User name: {user.displayName}</p>
+                <img className='rounded-circle' src={user.photoURL} alt="" />
+            </div>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
                 <Link className='btn btn-primary' to="/">Home</Link>
                 <Link className='btn btn-primary' to="/blog">Blog</Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <Link onClick={handleGoogleSignIn} className='btn btn-primary' to="/login">Log In</Link>
                   <Link className='btn btn-primary' to="/register">Register</Link>
-                  <Link className='btn btn-primary' to="#action/3.3">FAQ</Link>
+          
+                  {user.uid ?
+                    <button className='btn btn-primary' >Sign Out</button> :
+                    <>
+                      <button className='btn btn-primary' onClick={handleGoogleSignIn}>Google Sing In</button>
+                      <button className='btn btn-primary' onClick={handleGithubSignIn}>Github Sign IN</button>
+                    </>
+                  }
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
