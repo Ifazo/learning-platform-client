e92ee69671d76a6247e3ffb0052623857cfcb0f0
Ziaul Karim Ifaz <ziaulkarimifaz@gmail.com>
2022-10-28 23:47:34 +0600

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9j1KavWpspmQU-q1Vwc8whn35RDjSHvA",
-  authDomain: "learning-platform-server-side.firebaseapp.com",
-  projectId: "learning-platform-server-side",
-  storageBucket: "learning-platform-server-side.appspot.com",
-  messagingSenderId: "220221505149",
-  appId: "1:220221505149:web:812f2ae0c96ee6ec233b14"
+ apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
