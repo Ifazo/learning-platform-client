325c8f4526c253c98cf3cb7d6d3869225eac9055
ifazo <ziaulkarimifaz@gmail.com>
2022-10-28 12:23:31 +0600

    react-to-pdf added

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import ReactToPdf from 'react-to-pdf';
+
+const ref = React.createRef();
 
 const CardDetails = () => {
 
@@ -7,10 +10,19 @@ const CardDetails = () => {
     console.log(course);
     return (
         <div>
-            <div className='container-fluid'>
-                <img src={course.logo} className='w-25' alt="" />
-                <h6>Couse Name: {course.name}</h6>
-                <p>Couse Details: {course.description}</p>
+            <div>
+                <ReactToPdf targetRef={ref} filename="details-print.pdf">
+                    {({ toPdf }) => (
+                        <button onClick={toPdf}>Generate pdf</button>
+                    )}
+                </ReactToPdf>
+                <div className='w-100 h-auto' ref={ref} >
+                    <div className='container-fluid'>
+                        <img src={course.logo} className='w-25' alt="" />
+                        <h6>Couse Name: {course.name}</h6>
+                        <p>Couse Details: {course.description}</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
